2eb8c5e5e6df29607e1347247e3d718bbbc0f4fc
ashraf-vazeer <46555462+ashraf-vazeer@users.noreply.github.com>
2020-01-01 13:06:48 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+        "cicd/calculation"
+        "github.com/satori/go.uuid"
+        log "github.com/sirupsen/logrus"
+)
+
+func main() {
+        a, b := 5, 2
+        log.Printf("values: %v %v\n", a, b)
+        log.Println(calculation.Sum(a, b))
+        log.Println(calculation.Sub(a, b))
+        log.Println("New UUID: ", uuid.NewV4().String())
+}
